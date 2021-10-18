26d9b89b9578bdf22ff5e5212740266f1eca1376
Chal13W1zz <charles.muthii@student.moringaschool.com>
2021-10-18 08:42:36 +0300

    add request service

---
+++ src/app/services/user-request.service.ts
@@ -0,0 +1,24 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
+import { User } from '../classes/user';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserRequestService {
+
+  constructor(private http: HttpClient) {
+   }
+
+   queryUser(query:string){
+     let promise = new Promise((resolve, reject)=>{
+       this.http.get<User>(environment.apiUrl+query,{headers: new HttpHeaders({'Authorization': 'token ' + environment.apiKey})}).toPromise().then((response) =>{
+         resolve(response);
+       }, error=>{
+         reject(error);
+       } );
+     });
+     return promise;
+   }
+}
