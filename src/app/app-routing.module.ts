fcc2292acf718646c413c450bf75dffb2da376ad
Chal13W1zz <charles.muthii@student.moringaschool.com>
2021-10-18 17:28:01 +0300

    add details route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DetailsComponent } from './components/details/details.component';
+import { SearchComponent } from './components/search/search.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'details', component: DetailsComponent},
+  {path: '', component: SearchComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
